93c847e7bcdf3d795484863668722128aaa8c0ea
hiracmc <120374767+hiracmc@users.noreply.github.com>
2024-12-07 20:29:37 +0900

    Update ScriptForm.js

--- BP/ScriptForm.js
+++ BP/ScriptForm.js
@@ -1,1 +1,14 @@
+import { world } from "@minecraft/server";
+import { ActionFormData } from "@minecraft/server-ui";
 
+const form1 = new ActionFormData()
+    .title("Form Template")
+    .body("説明")
+    .button("Button1")
+    .button("Button2")
+    .button("Button3");
+
+world.afterEvents.itemUse.subscribe(ev => {
+    const { itemStack, source } = ev;
+        case "minecraft:compass" : form1.show(source); break;
+  });
