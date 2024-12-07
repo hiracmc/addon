023adc0675b9e579c4e59f548e98fb29194efd37
hiracmc <120374767+hiracmc@users.noreply.github.com>
2024-12-07 20:48:39 +0900

    Update chat_command.js

--- BP/scripts/chat_command.js
+++ BP/scripts/chat_command.js
@@ -1,1 +1,10 @@
+import { world } from "@minecraft/server";
 
+world.beforeEvents.chatSend.subscribe((ev) => {
+	const message = ev.message;
+			if (message = "!cheat") {
+        ev.cancel = true;
+        player.runCommandAsync('give @s diamond 64');
+			break;
+	}
+});
